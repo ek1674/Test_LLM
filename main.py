fbbe46d2ab871fea163991df06ebdc5bc91028bf
ek1674 <117951657+ek1674@users.noreply.github.com>
2023-09-22 00:28:45 -0700

    Current code

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #  This is a test program to see if by creating a new project the import errors are resolved
 
+import sys
+
+# Add the path to the spacy_transformers library (you may not need this if the library is installed in your virtual environment)
+# sys.path.append("C:\\Users\\kroms\\.conda\\envs\\LLM_Test\\Lib\\site-packages\\spacy_transformers-1.2.5.dist-info")
+
 import spacy
-from spacy_transformers import TransformersLanguage
+import spacy_transformers
+from spacy_transformers import Transformers
+from spacy_transformers import TransformersWordPiercer, TransformersTok2Vec
 import tensorflow as tf
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlsparse, urljoin
+from urllib.parse import urljoin
 from transformers import AutoTokenizer
-#from googlesearch import search
 
 #query = "Windows Logs"
 #for search_result in search (query, num_results=10):
@@ -18,10 +24,10 @@ from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 
 #  Load a Transformers pipeline with RoBERTa
-nlp = TransformersLanguage(trf_name="roberta-base", meta={"land": "en"})
+nlp = Transformers(trf_name="roberta-base", meta={"land": "en"})
 word_piecer = TransformersWordPiercer.from_pretrained("roberta-base")
 tok2vec = TransformersTok2Vec.from_pretrained("roberta-base")
-nlp.add_pipe(word_piercer, before="ner")
+nlp.add_pipe(word_piecer, before="ner")
 nlp.add_pipe(tok2vec, before="ner")
 
 
@@ -40,7 +46,7 @@ def scrape_web_data(url):
         #  Check is the request was successful
         if response.status_code == 200:
             #  Parse the HTML content of the page
-            soup = BeautifulSoup(response.text, 'htm;.parser')
+            soup = BeautifulSoup(response.text, 'html.parser')
 
             #  Extract text from the web page
             text = soup.get_text()
@@ -54,7 +60,7 @@ def scrape_web_data(url):
             #  Find and follow links on this page
             for link in soup.find_all('a'):
                 href = link.get('href')
-                if href and href.startswith(('httep://', 'https://')):
+                if href and href.startswith(('http://', 'https://')):
                     #  Ensure that the link is an absolute URL
                     next_url = href
                 else:
@@ -64,14 +70,14 @@ def scrape_web_data(url):
                 #  Check if the URL has been visited
                 if next_url not in visited_urls:
                     #  Recursively scrape the next URL
-                    scrape_web_page(next_url)
+                    scrape_web_data(next_url)
         #  Return the doc object after processing
         return doc
 
     except Exception as e:
         print("Error:", str(e))
 
-    #  Return one in the case of an error
+    #  Return None in the case of an error
     return None
 
 #  Specify the starting URL
@@ -80,14 +86,14 @@ start_url = "https://answers.microsoft.com/en-us/windows/forum/all/installation-
 #  Scrape and process web data
 doc = scrape_web_data(start_url)
 
-#  Use the scraped and processed data for training you LLM
+#  Use the scraped and processed data for training your LLM
 if doc:
     #  Access token-level information, excluding whitespace tokens
     for token in doc:
         if not token.is_space:
             print("Text:", token.text)
-            print("Lema:", token.lemma_)
-            print("Part of Speech:", token,pos_)
+            print("Lemma:", token.lemma_)
+            print("Part of Speech:", token.pos_)
             print("Tag:", token.tag_)
 else:
     print("No data returned from web scraping.")
